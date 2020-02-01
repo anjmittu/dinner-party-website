c6de26c8c3e3a00c2baa567ebd6cebc0be56eafa
Mysore-Ajay <Mysore_Ajay@bah.com>
2020-02-01 15:24:39 -0500

    "Fixed" Footer elements

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def layout():
 def createGroup():
     groupName = "GroupName"
     return render_template("group.html", groupName=groupName)
-   
+
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port="80")
+    app.run(debug=True, host='0.0.0.0', port=80)
