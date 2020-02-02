b0d9c007bf2454aecddff82229420e592db0e1ee
rdmittu <rdmittu@gmail.com>
2020-02-01 19:39:49 -0500

    a bunch of changes

--- groupForm.py
+++ groupForm.py
@@ -1,9 +1,20 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, BooleanField
+from wtforms import Form, FieldList, FormField, IntegerField, StringField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email
 
-class CreateGroupForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=15)])
-    email = StringField('Email', validators=[DataRequired(), Email()])
+class PersonForm(Form):
 
+    username = StringField('Username', validators=[DataRequired()])
+    phone_number = StringField("Phone Number", validators=[DataRequired])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    restrictions = StringField("Dietary Restrictions", validators=[])
+    isChef = BooleanField('Chef', validators=[])
+
+class CreateGroupForm(FlaskForm):
+
+    people = FieldList(
+        FormField(PersonForm),
+        min_entries=2,
+        max_entries=10
+    )
     submit = SubmitField('Create Group')
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect
 from groupForm import CreateGroupForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'b3cb272ad35cbeb8f11b88974fd34ba0'
 
 @app.route("/")
 @app.route("/index")
@@ -14,11 +16,15 @@ def layout():
     return render_template("layout.html")
 
 @app.route("/")
-@app.route("/createGroup")
+@app.route("/createGroup", methods=['GET', 'POST'])
 def createGroup():
+    form = CreateGroupForm()
     groupName = "GroupName"
-    return render_template("group.html", groupName=groupName)
-
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('index'))
+    return render_template("group.html", groupName=groupName, form=form)
+   
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(debug=True, host='0.0.0.0', port="80")
